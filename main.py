8b4e0e42a26d6d9636d7fa817e6e06ad1d611439
Quinn Montgomery <montgomeryq@wit.edu>
2020-12-22 18:25:23 -0500

    you can now log into spotify

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 from flask import Flask, render_template, send_file, redirect, request
-import subprocess, pickle, requests
+import subprocess, pickle, requests, base64, json
+import userData
 
 app = Flask(__name__)
 client_id = 'd9f97736297e4a039202cb31e162c0ef'
 client_secret = '50575c7c28be4b03863f6781d0c5ad46'
 response_type = 'code'
-redirect_uri = 'http%3A%2F%2Flocalhost%3A5000%2Fcallback'
+redirect_uri = 'http://localhost:5000/callback'
 scope = 'user-read-private%20user-read-email'
-user_token = ''
+user_data = None
 browser = 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
 
 @app.route('/')
 def login():
 
     try:
-        user_token = pickle.load(open('user_token.p', 'rb'))
+        user_data = pickle.load(open('user_data.p', 'rb'))
     except:
         subprocess.run([
             "C:\\Program Files\\Mozilla Firefox\\firefox.exe",
-            f"https://accounts.spotify.com/authorize?client_id={client_id}&response_type={response_type}&redirect_uri={redirect_uri}&scope={scope}"
+            f"https://accounts.spotify.com/authorize?client_id={client_id}&response_type={response_type}&redirect_uri={redirect_uri.replace('/', '%2F').replace(':', '%3A')}&scope={scope}"
         ])
 
     return redirect('/index.html')
@@ -37,11 +38,41 @@ def pictures(picture):
 
 @app.route('/callback')
 def callback():
-    user_token = request.args.get('code')
 
-    pickle.dump(user_token, open('user_token.p', 'wb+'))
+    user_data = userData.userData()
 
-    return 'Token was sucsesfully saved, you may close this tab :)'
+    user_data.user_key = request.args.get('code')
+
+    #get token and refresh
+
+    encoded_ids = base64.b64encode(bytes(f"{client_id}:{client_secret}", "ascii")).decode('ascii')
+
+    spotify_request_result = requests.post(
+        'https://accounts.spotify.com/api/token',
+        
+        data = {'Content-Type':'application/x-www-form-urlencoded', 
+                  'grant_type':'authorization_code', 
+                  'code':user_data.user_key, 
+                  'redirect_uri':redirect_uri},
+
+        headers = {'Authorization':f'Basic {encoded_ids}'}
+                
+    )
+
+    if spotify_request_result.status_code == 200:
+
+        #save user data
+
+        user_data.user_token = spotify_request_result.json()['access_token']
+        user_data.user_refresh = spotify_request_result.json()['refresh_token']
+        user_data.scope = spotify_request_result.json()['scope']
+
+        pickle.dump(user_data, open('user_data.p', 'wb+'))
+        return 'Token was sucsesfully saved, you may close this tab :)'
+
+    else:
+        print(spotify_request_result.json)
+        return spotify_request_result.text
 
 print(__name__)
 if __name__ == '__main__':
---
+++ userData.py
@@ -0,0 +1,7 @@
+
+class userData:
+    #user access info
+    user_key = None
+    user_token = None
+    user_refresh = None
+    scope = None
